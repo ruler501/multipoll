12340c3e24705962566200d0b4b7e67c86f822a5
Devon Richards <friends.devon@gmail.com>
2019-12-02 16:19:47 -0600

    Actually use system for displaying results.

--- multipoll/templatetags/electoral_system.py
+++ multipoll/templatetags/electoral_system.py
@@ -1,6 +1,9 @@
+from typing import List
+
 from django import template
 
 from multipoll.electoralsystems import get_electoral_system
+from multipoll.models import PollBase
 
 register = template.Library()
 
@@ -8,3 +11,8 @@ register = template.Library()
 @register.filter(name='system_name')
 def system_name(value: str) -> str:
     return get_electoral_system(value).label
+
+
+@register.filter(name='formatted_results')
+def formatted_results(value: PollBase, arg: str) -> List[str]:
+    return value.get_formatted_votes(arg)
